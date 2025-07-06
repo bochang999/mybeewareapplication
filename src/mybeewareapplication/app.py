4a14bcdc9a69acc8cb10045ed12fa44fe8526330
bochang999 <kirokiromushi@gmail.com>
2025-07-06 22:40:13 +0900

    Fix: app.py を修正

--- src/mybeewareapplication/app.py
+++ src/mybeewareapplication/app.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-"""
-A simple BeeWare application
-"""
-
-import toga
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
-
-
-class MyBeeWareApplication(toga.App):
-    def startup(self):
-        """Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
-
-
-def main():
-    return MyBeeWareApplication()
-=======
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, CENTER
@@ -55,4 +27,3 @@ class MyBeewareApplication(toga.App):
 
 def main():
     return MyBeewareApplication()
->>>>>>> 19df0c4 (Add basic counter app and docs)
